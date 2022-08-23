29f78f534a175357b0cd20dbf8526759277e1a96
Dmitry <romanovichd.hr@gmail.com>
2022-08-23 19:51:22 +0300

    Finish

---
+++ HomeWork2/2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+Console.WriteLine("Введите трехзначное число");
+string DeleteSecond()
+{
+    int a = int.Parse(Console.ReadLine());
+    int First = a / 100;
+    int Third = a % 10;
+    int desired = First * 10 + Third;
+
+    return ("ответ "+ desired);
+}
+Console.WriteLine(DeleteSecond());
---
+++ HomeWork2/2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на 
+// вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("введите число");
+string FindThird()
+{
+    int a = int.Parse(Console.ReadLine());
+    if (a < 8)
+    {
+        if (a < 6) return ("нет");
+        else return ("да");
+    }
+    else return ("такого дня недели нет");
+}
+Console.WriteLine(FindThird());
